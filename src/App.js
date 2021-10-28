c6a60fad958fec50e9660c08b4ee930cd6afa288
Shubham Bhavsar <shubhambhavsar50@gmail.com>
2021-10-28 15:17:45 +1100

    Initialised an app with home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Typography,
+  AppBar,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Stack,
+  Box,
+  Toolbar,
+  Container,
+  Link,
+} from "@mui/material";
+import CakeSharpIcon from "@mui/icons-material/CakeSharp";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <CakeSharpIcon />
+          <Typography variant="h6">Cake House</Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <Container maxWidth="sm">
+          <Typography
+            variant="h2"
+            align="center"
+            color="text.primary"
+            gutterBottom
+          >
+            Cake Gallery
+          </Typography>
+          <Typography
+            variant="h5"
+            align="center"
+            color="text.secondary"
+            paragraph
+          >
+            All our cakes are home-made and every ingredient is chosen carefully
+            by keeping your health in mind!
+          </Typography>
+        </Container>
+      </main>
+    </>
   );
-}
+};
 
 export default App;
